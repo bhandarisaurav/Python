b763db923c45b7feaada1ccfd7638f2347fd4409
saurav529 <saurav.bhandari@deerwalk.edu.np>
2017-07-22 09:07:11 +0545

    Initial

--- FirstClass.py
+++ FirstClass.py
@@ -1,17 +1,52 @@
-x =10;
-print(x);
+x =10
+print(x)
 
-print("Saurav Bhandari\n");
+print("Saurav Bhandari\n")
 
-print("Saurav Bhandari\n");
+print("Saurav Bhandari\n")
 
-name = "saurav";
+name = "saurav"
 
-print (name[0]);
-print (name[2:4]);
-print (name[:4]);
-print (name[4:]);
-print (name[-4:1]);
-print (name[-4:-1]);
-print (name *2);
-print (name + '!!!');
+# print (name[0])
+# print (name[2:4])
+# print (name[:4])
+# print (name[4:])
+# print (name[-4:1])
+# print (name[-4:-1])
+# print (name *2)
+# print (name + '!!!')
+
+values=['abc','saurav',['nepal','japan','china'],'hari']
+
+len(values)
+
+values.extend([2])
+
+print(values)
+
+values.extend(['dhiraj','dhungana'])
+
+print(values)
+
+values.append([2,3,4,5,6,7,8,9])
+
+print(values)
+
+person = {"name":'ram',"age":"12"}
+print(person['name'])
+
+dictionary = {'01': "room1",'02':"room2","977":"nepal"}
+
+print(dictionary)
+
+print(dictionary["977"])
+
+p={"first": [1, 2, 3, 4, 5], "second": ["Saurav","Bhandari"],"third": ["Sulav","Prasain"]}
+
+print(p['second'])
+
+list = [{"first": [1, 2, 3, 4, 5]},99]
+
+print(list)
+
+print(list[0]['first'])
