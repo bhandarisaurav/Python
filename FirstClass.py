59f1a7c9bc2d3f6a1f4b1aef8c6abed12461e736
saurav529 <saurav.bhandari@deerwalk.edu.np>
2017-07-22 08:09:05 +0545

    Initial

---
+++ FirstClass.py
@@ -0,0 +1,17 @@
+x =10;
+print(x);
+
+print("Saurav Bhandari\n");
+
+print("Saurav Bhandari\n");
+
+name = "saurav";
+
+print (name[0]);
+print (name[2:4]);
+print (name[:4]);
+print (name[4:]);
+print (name[-4:1]);
+print (name[-4:-1]);
+print (name *2);
+print (name + '!!!');
--- saurav.py
+++
@@ -1,6 +0,0 @@
-x =10;
-print(x);
-
-print("Saurav Bhandari\n");
-
-print("Saurav Bhandari\n");
