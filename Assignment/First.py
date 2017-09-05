facb48263f97e06cacae484d12fe7f8935df9543
saurav529 <saurav.bhandari@deerwalk.edu.np>
2017-09-05 15:07:43 +0545

    Assignment Update

--- Assignment/First.py
+++ Assignment/First.py
@@ -3,22 +3,34 @@ import os
 os.system('cls')
 
 
-def display():
+def menu():
     print("Enter Your Academic Details")
     name = input("Enter Name: ")
     age = input("Enter Age: ")
-    address = input("Enter Address: ")
+    add = input("Enter Address: ")
+    address = list(map(str, add.split()))
+    x = int(address[3])
+    address[3] = x
+    print(address)
     college = input("Enter College Name: ")
     level = input("Enter Study Level: ")
     course = input("Enter Course: ")
+    course = tuple(map(str, course.split()))
+    dictionary = {'name': name, 'age': age, 'address': address, 'college': college, 'level': level, 'course': course}
+    x = 1
+    data = {x: dictionary}
+    x += 1
+    return data
 
+
+def display_data(data):
+    print(data)
 
 
 if __name__ == "__main__":
-    data = []
-    input("How Many")
-    for i in range(0, 10):
+    n = int(input("How Many Data you want to Insert : "))
+    os.system("cls")
+    for i in range(0, n):
         os.system("cls")
-        display()
-        dictionary = {}
-        list.append(dictionary)
+        x = menu()
+    display_data(x)
